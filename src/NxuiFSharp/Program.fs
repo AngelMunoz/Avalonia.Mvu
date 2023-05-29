19ccdbaef0b717bb6f7d76fde8b18ccfca8638ad
Angel D. Munoz <scythes.lol@gmail.com>
2023-05-29 14:09:21 -0600

    Move app code to Program.fs

--- src/NxuiFSharp/Program.fs
+++ src/NxuiFSharp/Program.fs
@@ -1,11 +1,43 @@
 ﻿open Avalonia
+open Avalonia.Data
+open Avalonia.Controls
+
 open NXUI.Extensions
+open type NXUI.Builders
+
 open NxUIFSharp
 
-AppBuilder
-    .Configure<Application>()
-    .UsePlatformDetect()
-    .UseFluentTheme()
-    .WithApplicationName("NXUI and F#")
-    .StartWithClassicDesktopLifetime(Counter.view, [||])
-|> ignore
+
+let panelContent (window: Window) =
+    let mutable count = 0
+    let button = Button().content ("NXUI + F#!")
+    let textbox = TextBox().text (window.BindTitle())
+
+    let label =
+        Label()
+            .content (
+                button.ObserveOnClick()
+                |> Observable.tap (fun _ -> count <- count + 1)
+                |> Observable.map (fun _ -> $"You clicked %i{count} times"),
+                mode = BindingMode.OneWay
+            )
+
+    StackPanel().children (button, textbox, label)
+
+let view () : Window =
+    let mutable window = null
+
+    Window(&window)
+        .title("NXUI From F#")
+        .width(300)
+        .height(300)
+        .content (panelContent window)
+
+[<EntryPoint>]
+let main argv =
+    AppBuilder
+        .Configure<Application>()
+        .UsePlatformDetect()
+        .UseFluentTheme(Styling.ThemeVariant.Dark)
+        .WithApplicationName("NXUI and F#")
+        .StartWithClassicDesktopLifetime(view, argv)
