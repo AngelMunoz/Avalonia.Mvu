314e83d14b8aad54ec0d55b308c9f09875784d6b
Angel D. Munoz <scythes.lol@gmail.com>
2023-05-29 14:09:13 -0600

    Rename to extensions

--- src/NxuiFSharp/Extensions.fs
+++ src/NxuiFSharp/Extensions.fs
@@ -1,13 +1,9 @@
-module NxUIFSharp.Counter
+namespace NxUIFSharp
 
 open System
 open Avalonia.Controls
 open Avalonia.Layout
 
-open NXUI
-open type NXUI.Properties
-open type NXUI.Events
-open type NXUI.Builders
 open NXUI.Extensions
 
 open System.Runtime.CompilerServices
@@ -76,28 +72,3 @@ type NxuiFsExtensions =
             [<ParamArray>] children: Control array
         ) : 'Type =
         PanelExtensions.Children(panel, children)
-
-let panelContent (window: Window) =
-    let mutable count = 0
-    let button = Button().content ("NXUI + F#!")
-    let textbox = TextBox().text (window.BindTitle())
-
-    let label =
-        Label()
-            .content (
-                button.ObserveOnClick()
-                |> Observable.tap (fun _ -> count <- count + 1)
-                |> Observable.map (fun _ -> $"You clicked %i{count} times"),
-                mode = Avalonia.Data.BindingMode.OneWay
-            )
-
-    StackPanel().children (button, textbox, label)
-
-let view () : Window =
-    let mutable window = null
-
-    Window(&window)
-        .title("NXUI From F#")
-        .width(300)
-        .height(300)
-        .content (panelContent window)
